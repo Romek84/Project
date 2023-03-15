0289a2da1666395c8837295dc2b02a707e627e04
Romek84 <118740987+Romek84@users.noreply.github.com>
2023-03-15 13:01:43 +0100

    Add files via upload

---
+++ breakfast_menu_all_week.py
@@ -0,0 +1,15 @@
+breakfast_menu_all_week = 4
+breakfast_menu_all_week == 7/1
+
+if breakfast_menu_all_week == 1:
+    print ("porridge with fruit")
+elif breakfast_menu_all_week == 2:
+    print("omelette with vegetables")
+elif breakfast_menu_all_week == 3:
+    print("sausages with bread")
+elif breakfast_menu_all_week == 4:
+    print("Scrambled eggs with bacon")
+elif breakfast_menu_all_week == 5:
+    print("three hard-boiled eggs")
+else:
+    print("we eat what we want. Joker day")
